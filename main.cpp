6b82d36a6dcc98e1412790c34cd2429ed9f7b694
borawskiD <borawskid@icloud.com>
2022-03-18 20:53:47 +0100

    correct version of swapDown is ready

--- main.cpp
+++ main.cpp
@@ -22,10 +22,9 @@ void tabExpander(int *&values, int &size, int element){
 void swapDown(int index,int *&heap, int size){
     int left = 2 * index + 1;
     int right = 2 * index + 2;
-
     int helper;
-    int max;
-    if (index < size){
+    int max = index;
+    if(index < size){
         std::cout<<"Wyglad tablicy przed ewentualnym przesunieciem: "<<std::endl;
         tabDisplayer(heap,size);
         std::cout<<std::endl;
@@ -35,26 +34,32 @@ void swapDown(int index,int *&heap, int size){
         if (left < size && heap[index] < heap[left]){
             std::cout<<"Lewy potomek spełnia warunki przesunięcia, bo: " <<heap[index]<<"<"<<heap[left]<<std::endl;
             max = left;
-            helper = heap[index];
-            heap[index] = heap[left];
-            heap[left] = helper;
         }
-        if (right < size & heap[index] < heap[right]){
+        if (right < size && heap[max] < heap[right]){
             std::cout<<"Prawy potomek spełnia warunki przesunięcia, bo: " <<heap[index]<<"<"<<heap[right]<<std::endl;
             max = right;
-            helper = heap[index];
-            heap[index] = heap[right];
-            heap[right] = helper;
         }
-        std::cout<<"Tablica po naszych działaniach wyglada następujaco:"<<std::endl;
-        tabDisplayer(heap,size);
-        std::cout<<std::endl<<"---"<<std::endl;
-        std::cout<<std::endl;
-        int * tab = (int *) heap;
-        swapDown(max,tab,size);
+        if (max != index){
+            helper = heap[max];
+            heap[max] = heap[index];
+            heap[index] = helper;
+            int * tab = (int *) heap;
+            std::cout<<"Tablica po naszych działaniach wyglada następujaco:"<<std::endl;
+            tabDisplayer(heap,size);
+            std::cout<<std::endl<<"---"<<std::endl;
+            std::cout<<std::endl;
+            swapDown(max,tab,size);
+        }
     }
-}
 
+    std::cout<<"Tablica po naszych działaniach wyglada następujaco:"<<std::endl;
+    tabDisplayer(heap,size);
+    std::cout<<std::endl<<"---"<<std::endl;
+    std::cout<<std::endl;
+}
+void heapify(int index,int *&heap, int size, int whereAmI){
+
+}
 int main() {
     int values[] = {5, 17, 12, 13, 6, 9, 10, 7, 11, 4, 2, 8};
     int size = sizeof(values)/sizeof(int);
